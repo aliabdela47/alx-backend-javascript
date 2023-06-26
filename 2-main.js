10f19700fb52fe4e01d137d7b085fb9deb8a4c61
Ali Abdela Ali <ardii.2000@gmail.com>
2023-06-26 22:24:18 +0300

    Delete 2-main.js

--- 2-main.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable new-cap */
-/* eslint-disable import/extensions */
-import getNeighborhoodsList from './2-arrow.js';
-
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
